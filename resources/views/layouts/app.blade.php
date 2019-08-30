3ad5fdcd7995e8b60fd346abe103754230750f76
Chechoelgris <so.sepulveda.m@gmail.com>
2019-08-30 01:16:17 -0400

    + img

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,8 +18,10 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
 </head>
-<body class="">
+<body class="body-bg-1">
+    
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
