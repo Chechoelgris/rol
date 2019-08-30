c4de1fb8ea75bd6606b79f46acaf696c3938b3b4
Chechoelgris <so.sepulveda.m@gmail.com>
2019-08-30 17:50:31 -0400

    agregados elementos de muestra a la vista home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="">
     <div class="row justify-content-center ">
-        <div class="col-md-3">
+        <div class="col-md-4">
             <div class="border border-success rounded text-light bg-dark-t m-4">
                     <div class="card-header ">
                     <div class="row  justify-content-between container">
@@ -17,22 +17,21 @@
 
                 <hr class="bg-success">
                 <div class="card-body ">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                   
 
-                    You are logged in!
+                    <p>You are logged in!</p>
+                    <p>You are logged in!</p>
+                    <p>You are logged in!</p>
+                    <p>You are logged in!</p>
                 </div>
             </div>
         </div>
-        <div class="col-md-7">
+        <div class="col-md-8">
                 <div class="border border-success rounded text-light bg-dark-t m-4">
                         <div class="card-header ">
                             <div class="row  justify-content-between container">
 
-                                    <div class="">Personajes</div>
+                                    <div class="">Aventuras</div>
                                     <div><a href="#" class="btn btn-outline-success">Detalles</a></div>
                             </div>
                             
@@ -46,6 +45,67 @@
                         </div>
                     </div>
         </div>
+    </div>
+
+    <div class="row justify-content-center ">
+        
+        
+        <div class="col-md-4">
+                <div class="border border-success rounded text-light bg-dark-t m-4">
+                        <div class="card-header ">
+                            <div class="row  justify-content-between container">
+                                    <div class="">Enemigos</div>
+                                    <div><a href="#" class="btn btn-outline-success">Detalles</a></div>
+                            </div>
+                        </div>
+
+                        <hr class="bg-success">
+
+                        <div class="card-body ">
+                            You are logged in!
+                            
+                            You are logged in!
+                        </div>
+                </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="border border-success rounded text-light bg-dark-t m-4">
+                    <div class="card-header ">
+                        <div class="row  justify-content-between container">
+                                <div class="">Personajes</div>
+                                <div><a href="#" class="btn btn-outline-success">Detalles</a></div>
+                        </div>
+                    </div>
+
+                    <hr class="bg-success">
+
+                    <div class="card-body ">
+                        You are logged in!
+                        
+                        You are logged in!
+                    </div>
+            </div>
+    </div>
+
+    <div class="col-md-4">
+        <div class="border border-success rounded text-light bg-dark-t m-4">
+                <div class="card-header ">
+                    <div class="row  justify-content-between container">
+                            <div class="">Diario</div>
+                            <div><a href="#" class="btn btn-outline-success">Detalles</a></div>
+                    </div>
+                </div>
+
+                <hr class="bg-success">
+
+                <div class="card-body ">
+                    You are logged in!
+                    
+                    You are logged in!
+                </div>
+        </div>
+</div>
     </div>
 </div>
 @endsection
